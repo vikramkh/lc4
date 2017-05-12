4fd59fd81db2479af2e271758124919d94211f77
Vikram Khemlani <vikramkh@sas.upenn.edu>
2017-05-12 11:01:15 -0500

    second commit

---
+++ milestone_file.h
@@ -0,0 +1,14 @@
+/*
+ * CIS 240 HW 10: LC4 Simulator
+ * milestone_file.h - prototype functions for milestone_file.c
+ */
+
+#include "object_files.h"
+
+/*
+ * Call this function to output the ASCII file: milestone.txt
+ * Return a zero if successful and a non-zero error code if you encounter a
+ * problem.
+ * Params: pointer to current machine state.
+ */
+int generate_milestone_file (lc4_state* state) ;
